a1b8a69cce69bf115105ddd23b050644fe7dd0d0
Sumitraj00736 <sumitraj00736@gmanil.com>
2024-03-28 20:32:21 +0530

    api respone  request and error formating

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 
-dotenv.config({
-    path: './env'
-})
+dotenv.config({path: './env'})
 
 connectDB()
-
+.then(()=>{
+    app.on("error", (error) =>{
+        console.log("ERROR: ", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO_DB connection failed !!! ", err); 
+})
 
 
 
